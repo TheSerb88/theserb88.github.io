7c542ebc4cf77400f9dc46ddec43cb8a0b6a353a
unknown <Cory@.(none)>
2014-04-05 17:24:02 -0500

    ver. 0.0.0405

--- game.js
+++ game.js
@@ -9,48 +9,94 @@ var hero = {
 	atk: 1,
 	def: 1,
 	hp: 10,
+	maxHp:10,
+	recovery: {
+		amt: 1,
+		rate: 1000
+	},
 	exp: 0,
 	gold: 0	
 };
 
 //STARTUP
 $(document).ready(function () {
+	$( "#dialog-form" ).dialog({
+      autoOpen: false,
+      height: 300,
+      width: 350,
+      modal: true,
+      buttons: {
+        "OK": function() {
+          if ($("#name").val() != ""){
+          		hero.name = $("#name").val();
+          	}
+          	$( this ).dialog( "close" );
+          	updateValues();
+          }
+        }
+      });	
+	
+	//LOAD
+	$( "#dialog-form" ).dialog( "open" );
     updateValues();
     
     //CLICKING
 	$('#fight').click(function() {
-		var monster = new creature("Green Slime", 3, 1, 0, 1);
-		fight(hero,monster);
-		hero.exp = hero.exp + 1;
-		updateValues();
+		var monster = new creature("Green Slime", 3, 1, 0, 1,1);
+		fight(hero,monster);		
 	});
+	
+	var fillHealth = setInterval(function () {
+		hero.hp += hero.recovery.amt;
+		if(hero.hp > hero.maxHp) {hero.hp = hero.maxHp;}
+		updateHP();
+	}, hero.recovery.rate);
+
+	
 });
 
 function updateValues() {
-	document.getElementById("heroName").innerHTML = hero.name;
-	document.getElementById("heroLevel").innerHTML = hero.level;
-	document.getElementById("heroHP").innerHTML = hero.hp;
-	document.getElementById("heroExp").innerHTML = hero.exp;
-	document.getElementById("heroGold").innerHTML = hero.gold;
+	$("#heroName").html(hero.name);
+	$("#heroLevel").html(hero.level);
+	//$("#heroHP").html(hero.hp);
+	$("#heroExp").html(hero.exp);
+	$("#heroGold").html(hero.gold);
+	updateHP();
 };
 
+function updateHP() {
+	var heroHp = $( "#heroHp" ),
+      progressLabel = $( ".progress-label" );
+ 
+    heroHp.progressbar({
+      value: hero.hp,
+      max: hero.maxHp
+    });
+    progressLabel.text("HP:" + hero.hp + "/" + hero.maxHp);
+};
+
+
 function fight(hero, creature){
-	document.getElementById("battle").innerHTML = "You found a " + creature.name + "!<br />";
+	$("#battle").html("You found a " + creature.name + "!<br />");
 	while(hero.hp > 0 && creature.hp > 0)
 	{
 		hero.hp -= creature.atk;
 		creature.hp -= hero.atk;	
-		document.getElementById("battle").innerHTML += "You hit the " + creature.name + " for " + hero.atk + " damage.<br />";
-		document.getElementById("battle").innerHTML += creature.name + " hit you for " + creature.atk + " damage.<br />";
+		$("#battle").append("You hit the " + creature.name + " for " + hero.atk + " damage.<br />");
+		$("#battle").append(creature.name + " hit you for " + creature.atk + " damage.<br />");
 			
 	}
 	if (hero.hp <= 0){
-		alert("you dead.");
+		$("#battle").append("You died and lost your gold.");
+		hero.gold = 0;
 	}
 	else{
-		document.getElementById("battle").innerHTML += "You killed the " + creature.name + "!<br />";
+		$("#battle").append("You killed the " + creature.name + "!<br />");
+		$("#battle").append("The " + creature.name + " dropped " + creature.goldDrop + " gold!<br />");
+		hero.exp += creature.exp ;
+		hero.gold += creature.goldDrop;
 	}
-	
+	updateValues();
 }
 
 
--- world.js
+++ world.js
@@ -2,10 +2,13 @@ var world = {
 	name: "Unknown World"
 };
 
-function creature(name,hp,atk,def,exp) {
+function creature(name,hp,atk,def,exp,gold) {
 	this.name = name;
 	this.hp = hp;
 	this.atk = atk;
 	this.def = def;
 	this.exp = exp;	
+	this.goldDrop = gold;
 }
+
+
